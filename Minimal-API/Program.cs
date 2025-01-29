8cbb4a0dd7634ee3c6d2e69a7a6aa91d8a65625d
David-Nakeeran <127234789+David-Nakeeran@users.noreply.github.com>
2025-01-29 17:14:23 +0000

    Added CRUD requests

--- Minimal-API/Program.cs
+++ Minimal-API/Program.cs
@@ -1,7 +1,29 @@
+using Microsoft.AspNetCore.Http.HttpResults;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
 var todos = new List<Todo>();
+
+app.MapGet("/todos", () => todos);
+app.MapGet("/todos/{id}", Results<Ok<Todo>, NotFound> (int id) =>
+{
+    var targetTodo = todos.SingleOrDefault(t => t.Id == id);
+    return targetTodo is null
+    ? TypedResults.NotFound()
+    : TypedResults.Ok(targetTodo);
+});
+app.MapDelete("/todos/{id}", (int id) =>
+{
+    todos.RemoveAll(t => t.Id == id);
+    return TypedResults.NoContent();
+});
+
+app.MapPost("/todos", (Todo task) =>
+{
+    todos.Add(task);
+    return TypedResults.Created("/todos{id}", task); // Returns todo object and http status code
+});
 
 app.Run();
 
